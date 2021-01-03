254610cd649d607fd7681fd6cc2505ed93381c9f
Beni9 <andre.benea@gmail.com>
2021-01-03 10:43:00 +0200

    initial commit

---
+++ src/app/recipes/recipes.component.ts
@@ -0,0 +1,16 @@
+import {Component} from '@angular/core';
+
+
+@Component({
+  selector: 'app-recipes',
+  templateUrl: './recipes.component.html'
+
+})
+
+
+export class RecipesComponent{
+
+
+
+}
+
